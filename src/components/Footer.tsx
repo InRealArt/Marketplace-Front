a200c645cfd06a7966f11acca42199c723e7beb8
gilbruno <gilles_bruno@gmx.fr>
2024-06-15 10:25:43 +0200

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,41 +20,32 @@ const Footer = () => {
         <nav className="Footer__links">
           <div>
             <h2 className="Footer__title">About</h2>
-            <Link className="Footer__link" href={'/'}>
-              Product
+            <Link className="Footer__link" href={'https://drive.google.com/file/d/1yBhAJDqSocNhLanwJoyiX1Y-Do9yJ3kL/view?pli=1'}>
+              White Paper
             </Link>
-            <Link className="Footer__link" href={'/'}>
-              Ressource
-            </Link>
-            <Link className="Footer__link" href={'/'}>
-              Term & Condition
-            </Link>
-            <Link className="Footer__link" href={'/'}>
+            <Link className="Footer__link" href={'https://www.inrealart.com/faq'}>
               FAQ
             </Link>
           </div>
           <div>
             <h2 className="Footer__title">Company</h2>
-            <Link className="Footer__link" href={'/'}>
+            <Link className="Footer__link" href={'https://www.inrealart.com/home#team'}>
               Our Team
             </Link>
-            <Link className="Footer__link" href={'/'}>
-              Partener
+            <Link className="Footer__link" href={'https://www.inrealart.com/home#partners'}>
+              Partners
             </Link>
-            <Link className="Footer__link" href={'/'}>
-              Privacy & Policy
-            </Link>
-            <Link className="Footer__link" href={'/'}>
-              Features
+            <Link className="Footer__link" href={'https://www.inrealart.com/tos'}>
+              Terms of service
             </Link>
           </div>
           <div>
             <h2 className="Footer__title">Contact</h2>
-            <a className="Footer__link" href="tel:+3310000000">
-              01 00 00 00 00
+            <a className="Footer__link" href="">
+              Paris  
             </a>
-            <a className="Footer__link" href="mailto:inrealart@gmail.com">
-              Inrealart@gmail.com
+            <a className="Footer__link" href="mailto:teaminrealart@gmail.com">
+              teaminrealart@gmail.com
             </a>
           </div>
         </nav>
