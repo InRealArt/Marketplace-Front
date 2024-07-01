441b3ef5fb3b5d8f1177813091a03ccc622ee0e0
gilbruno <gilles_bruno@gmx.fr>
2024-07-01 21:02:48 +0200

    add cache buster for images

--- src/utils/getImageFromUri.ts
+++ src/utils/getImageFromUri.ts
@@ -1,3 +1,8 @@
+const generateRandomFiveDigitNumber = (): number => {
+  return Math.floor(10000 + Math.random() * 90000);
+}
+
 export const getImageFromUri = (uri: string) => {
-  return `${process.env.NEXT_PUBLIC_PINATA_URL}/ipfs/${uri}`
+  const cacheBuster = generateRandomFiveDigitNumber()
+  return `${process.env.NEXT_PUBLIC_PINATA_URL}/ipfs/${uri}?cache_buster=${cacheBuster}`
 }
